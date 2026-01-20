a7342f7302f054688ae74ce36169394fea2bdf9e
Marjorie Ortola <marjorie.ortola@oclock.school>
2026-01-20 17:54:01 +0100

    inscription coté back

--- api/app.js
+++ api/app.js
@@ -1,11 +1,10 @@
 import express from "express";
-
+import cors from 'cors';
 const app = express();
 
 
 app.use(express.json()); // 🔥 INDISPENSABLE
 
-import cors from 'cors';
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
