a151286c9f4e803db7c49d7b535bcd834ce15281
Marjorie Ortola <maraux.marjorie@hotmail.fr>
2026-03-01 15:27:38 +0100

    mise en place du .env et url pour déploiement

--- api/app.js
+++ api/app.js
@@ -1,32 +1,30 @@
 // Load environnment variables from .env
-import 'dotenv/config';
+import "dotenv/config";
 // Import NPM modules
 import express from "express";
 
-
 // Import le middleware qui vérifie les tokens
-import { validateToken } from './middlewares/auth.middleware.js';
+import { validateToken } from "./middlewares/auth.middleware.js";
 
 // Import le middleware qui protège accès admin
 import adminMiddleware from "./middlewares/admin.middleware.js";
 
 // Import local modules
-import activityRouter from './routes/activity.router.js';
-import authRouter from './routes/auth.router.js';
-import reservationRouter from './routes/reservation.router.js';
-import adminRouter from './routes/admin.router.js'
+import activityRouter from "./routes/activity.router.js";
+import authRouter from "./routes/auth.router.js";
+import reservationRouter from "./routes/reservation.router.js";
+import adminRouter from "./routes/admin.router.js";
 
-
-// import cors  
-import cors from 'cors';
+// import cors
+import cors from "cors";
 // import xss sanitizer
-import { xss } from 'express-xss-sanitizer';
+import { xss } from "express-xss-sanitizer";
 
 // Create Express app
 const app = express();
 // middleware cors
 // Accepte de partager les données avec localhost:5173
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ origin: "https://zombieland-1.onrender.com" }));
 app.use(express.urlencoded({ extended: true }));
 
 // Indique à express qu'on utiliser du JSON dans le body des requetes et des reponses HTTP
@@ -48,8 +46,8 @@ app.use(validateToken);
 app.use(adminMiddleware);
 app.use(adminRouter);
 //Route test
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.use((err, req, res, next) => {
@@ -62,5 +60,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`🚀 API started at http://localhost:${PORT}`);
+  console.log(`🚀 API started at http://localhost:${PORT}`);
 });
